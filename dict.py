540d7581b1d3a0cc1057e965fea53c3238980206
Rishanasiddique <51260379+Rishanasiddique@users.noreply.github.com>
2020-06-09 14:29:38 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+data =json.load(open("data.json"))
+def translate(word):
+    word=word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word,data.keys()))>0:
+        print("did you mean %s instead" %get_close_matches(word,data.keys())[0])
+        decide=input("press for y for yes or n for no : ")
+        if decide=="y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif decide=="n":
+            print("you have entered wrong word")
+        else:
+            print("you have entered wrong input")
+    else:
+        print("you have entered wrong word")
+
+
+word=input("Enter the word you want to search : ")
+output=translate(word)
+if type(output)==list:
+    for item in output:
+        print(item)
+else:
+    print(output)
